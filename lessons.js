a70bd855043c071be5470550c2166cdfa53a1739
rehamsalman <you@example.com>
2020-11-15 03:11:35 +0400

    Lesson js file with json objects

---
+++ lessons.js
@@ -0,0 +1,10 @@
+let lessons=[{
+    id:0,
+    name:"Cinematography",
+    details:"2 weeks self paced learning ",
+    image:"https://i.pinimg.com/originals/da/c5/67/dac56711e75ef7ec7f194f2adcb0fdd0.png",
+    price:"$120" ,
+    Location:"Dubai,UAE",
+    inventory:5,
+
+}]
